a8e4d49cee612f9559ed408c12e092687740c446
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-04-03 16:40:23 +0200

    add switch (multiplication)

--- src/FindQuarterRunner.java
+++ src/FindQuarterRunner.java
@@ -22,6 +22,34 @@ public class FindQuarterRunner {
     BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
 
     Point checkPoint = Point.read(read);
+
+    int multiplication = (int) Math.signum(checkPoint.getX() * checkPoint.getY());
+
+    switch (multiplication) {
+      case 1:
+        if (checkPoint.getX() > 0) {
+          System.out.println("Точка находиться в первом квадрате");
+        } else {
+          System.out.println("Точка находиться в третьем квадрате");
+        }
+        break;
+      case -1:
+        if (checkPoint.getX() > 0) {
+          System.out.println("Точка находиться в четвертом квадрате");
+        } else {
+          System.out.println("Точка находиться во втором квадрате");
+        }
+        break;
+      case 0:
+        if (checkPoint.getX() == 0 && (checkPoint.getY() == 0)) {
+          System.out.println("Точка находиться в центре координат");
+        } else if (checkPoint.getX() == 0) {
+          System.out.println("Точка лежит на оси x");
+        } else {
+          System.out.println("Точка лежит на оси y");
+        }
+    }
+
     System.out.println(checkPoint);
   }
 
